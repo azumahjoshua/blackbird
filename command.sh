20e1771cd6d1393ba0b1f452b1eb2c59f06ea83e
azumahjoshua <joshuaazumah1086@gmail.com>
2022-11-18 14:16:05 +0100

    update_logo and url

--- command.sh
+++ command.sh
@@ -3,7 +3,7 @@ npx create-react-app blackbird && cd blackbird && npm start
 git commit -m 'firstcommit'
 
 git checkout update_logo
-
+git add .
 git commit -m "update_logo and url"
 git push origin update_logo
 
